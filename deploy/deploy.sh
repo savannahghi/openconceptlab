352d16cd9428ba2d9e71ce0fdce1c2f44d99eb83
Salaton <nairouasalaton@gmail.com>
2024-05-07 11:19:04 +0300

    deploy(test): test ocl deployment with increased resource allocation

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -30,9 +30,12 @@ helm upgrade \
     --set app.container.env.dbHost="${DB_HOST}"\
     --set app.container.env.dbPort="${DB_PORT}"\
     --set app.container.env.db="${DB}"\
+    --set app.container.env.dbUser="${DB_USER}"\
     --set app.container.env.dbPassword="${DB_PASSWORD}"\
-    --set app.container.env.elasticSearchHost="${ES_HOST}"\
+    --set app.container.env.elasticSearchHost="${ES_HOSTS}"\
     --set app.container.env.elasticSearchPort="${ES_PORT}"\
+    --set app.container.env.elasticSearchUser="${ES_USER}"\
+    --set app.container.env.elasticSearchPassword="${ES_PASSWORD}"\
     --set app.container.env.flowerPassword="${FLOWER_PASSWORD}"\
     --set app.container.env.flowerUser="${FLOWER_USER}"\
     --set app.container.env.flowerHost="${FLOWER_HOST}"\
